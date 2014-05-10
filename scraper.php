8fc1a317a6a0d352042e2fa7a59052ff1f1f4d57
Madouc <bas.withagen@gmail.com>
2014-05-10 20:23:44 +0000

    Update scraper.php
    another typo...

--- scraper.php
+++ scraper.php
@@ -36,7 +36,7 @@ foreach($dom->find("#content .wikitable tr") as $data)
     $lng = "";
     //echo "$locationName\n";
     $geocode_url = "http://where.yahooapis.com/v1/places.q('";
-    $app_id = "')?appid=DX4mM4PV34ESO96yg70UGL5nu87SZ.gLXnubndwBjFvVp6_6LlnRfyd7Co_4s_W1q3se1LE-"
+    $app_id = "')?appid=DX4mM4PV34ESO96yg70UGL5nu87SZ.gLXnubndwBjFvVp6_6LlnRfyd7Co_4s_W1q3se1LE-";
     //$geocode_url = 'http://open.mapquestapi.com/nominatim/v1/search?format=json&q=';
     /*print("\n    geocode_url: ".$geocode_url.$combinedLocationQuery);*/
     $geoResult = file_get_contents($geocode_url.$combinedLocationQuery.$app_id);
